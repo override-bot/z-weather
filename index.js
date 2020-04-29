efbbe6b23e4d75408b30fdedfbf5e5d5d9c721ac
Chukwudi Wisdom <override-bot@users.noreply.github.com>
2020-04-29 20:01:34 +0200

    hei

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 let key = "8ae21bd13ed8a84eb5455a3699b91c3d";
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else if (!(navigator.geolocation)) {
+        console.log('location not found');
+    }
+}
+
+function showPosition(position) {
+    window.lat = position.coords.latitude;
+    window.lon = position.coords.longitude;
+    window.alert("Location Acquired: lat:" + lat + " lng:" + lon);
+    parseJson();
+}
+
 function parseJson() {
     //var symbol = document.getElementById('symbol').value;
-    let link = "https://api.openweathermap.org/data/2.5/weather?q=enugu&units=metric&apikey=8ae21bd13ed8a84eb5455a3699b91c3d";
-    var cityName = 'enugu';
+    let link = "https://api.openweathermap.org/data/2.5/weather?lat=" + window.lat + "&lon=" + window.lon + "139&units=metric&apikey=8ae21bd13ed8a84eb5455a3699b91c3d";
     let request = new XMLHttpRequest();
     request.open('GET', link);
     request.responseType = 'json';
@@ -16,13 +30,13 @@ function parseJson() {
     }
 }
 
+
 function showWeather(response) {
     var container = document.getElementById('data');
     var image = document.createElement('img');
-    image.className = 'image';
     image.src = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
 
-
+    image.className = 'image';
     container.appendChild(image);
     var temp = document.createElement('p')
     temp.className = 'temp';
