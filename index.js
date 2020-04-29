c8d7ca06d1e4b3778480e3c558328d31faafa54f
Chukwudi Wisdom <override-bot@users.noreply.github.com>
2020-04-29 12:01:29 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let key = "ce2b708b22a04a488bd5ce83e7889109";
 
 function parseJson() {
     //var symbol = document.getElementById('symbol').value;
-    let link = "https://api.weatherbit.io/v2.0/current?city=italy&key=ce2b708b22a04a488bd5ce83e7889109";
+    let link = "http://api.weatherstack.com/current?access_key=700caecc3ef3ca305c1308d2e08a8c3f&query=enugu";
 
     let request = new XMLHttpRequest();
     request.open('GET', link);
@@ -18,25 +18,24 @@ function parseJson() {
 
 function showWeather(response) {
     var container = document.getElementById('data');
-    var image = document.createElement('i');
-    /** if (response.data[0].weather.description == "Few clouds") {
-         image.src = "https://www.metaweather.com/static/img/weather/ico/hc.ico";
-     } else if (response.data[0].weather.description == "Clear Sky") {
-         image.src = "https://www.metaweather.com/static/img/weather/ico/c.ico"
-     } else if (response.data[0].weather.description == "Broken clouds") {
-         image.src = "https://www.metaweather.com/static/img/weather/ico/hc.ico"
-     } else if (response.data[0].weather.description == "Overcast clouds") {
-         image.src = "https://www.metaweather.com/static/img/weather/ico/hc.ico"
-     }**/
-    image.className = response.data[0].weather.icon;
+    var image = document.createElement('img');
+    image.src = response.current.weather_icons[0];
+
+    image.className = 'image';
     container.appendChild(image);
     var temp = document.createElement('p')
     temp.className = 'temp';
-    temp.innerHTML = `${response.data[0].temp}&#176`;
+    temp.innerHTML = `${response.current.temperature}&#176`;
     container.appendChild(temp);
     var city = document.createElement('p')
     city.className = 'city';
-    city.innerHTML = `${response.data[0].city_name}, ${response.data[0].country_code}`;
+    city.innerHTML = `${response.location.name}, ${response.location.country}`;
     container.appendChild(city);
+
+    document.getElementById('visibility').innerHTML = `${response.current.visibility}km`;
+    document.getElementById('speed').innerHTML = `${response.current.wind_speed}km/h`;
+    document.getElementById('cover').innerHTML = `${response.current.cloudcover}`;
+    document.getElementById('humid').innerHTML = `${response.current.humidity}`;
+
 
 }
