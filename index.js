f8024a337e6a3df7d005315dcc93f8cf5672ace2
Chukwudi Wisdom <override-bot@users.noreply.github.com>
2020-04-29 16:35:58 +0200

    hei God

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let key = "ce2b708b22a04a488bd5ce83e7889109";
+let key = "8ae21bd13ed8a84eb5455a3699b91c3d";
 
 function parseJson() {
     //var symbol = document.getElementById('symbol').value;
-    let link = "http://api.weatherstack.com/current?access_key=700caecc3ef3ca305c1308d2e08a8c3f&query=enugu";
-
+    let link = "https://api.openweathermap.org/data/2.5/weather?q=enugu&units=metric&apikey=8ae21bd13ed8a84eb5455a3699b91c3d";
+    var cityName = 'enugu';
     let request = new XMLHttpRequest();
     request.open('GET', link);
     request.responseType = 'json';
@@ -19,23 +19,23 @@ function parseJson() {
 function showWeather(response) {
     var container = document.getElementById('data');
     var image = document.createElement('img');
-    image.src = response.current.weather_icons[0];
+    image.src = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
 
     image.className = 'image';
     container.appendChild(image);
     var temp = document.createElement('p')
     temp.className = 'temp';
-    temp.innerHTML = `${response.current.temperature}&#176`;
+    temp.innerHTML = `${response.main.temp}&#176`;
     container.appendChild(temp);
     var city = document.createElement('p')
     city.className = 'city';
-    city.innerHTML = `${response.location.name}, ${response.location.country}`;
+    city.innerHTML = `${response.name}, ${response.sys.country}`;
     container.appendChild(city);
 
-    document.getElementById('visibility').innerHTML = `${response.current.visibility}km`;
-    document.getElementById('speed').innerHTML = `${response.current.wind_speed}km/h`;
-    document.getElementById('cover').innerHTML = `${response.current.cloudcover}`;
-    document.getElementById('humid').innerHTML = `${response.current.humidity}`;
+    document.getElementById('visibility').innerHTML = `${response.wind.deg}&#176`;
+    document.getElementById('speed').innerHTML = `${response.wind.speed}km/h`;
+    document.getElementById('cover').innerHTML = `${response.main.pressure}`;
+    document.getElementById('humid').innerHTML = `${response.main.humidity}`;
 
 
 }
